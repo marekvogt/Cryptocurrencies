6f1b74f1d310d605bc59d0f56c42abbe1dd19827
Marek <maarekvogt@gmail.com>
2019-09-25 20:11:09 +0200

    setup navigation with toolbar

--- app/src/main/java/pl/marekvogt/cryptocurrency/ui/MainActivity.kt
+++ app/src/main/java/pl/marekvogt/cryptocurrency/ui/MainActivity.kt
@@ -1,13 +1,29 @@
 package pl.marekvogt.cryptocurrency.ui
 
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 import dagger.android.support.DaggerAppCompatActivity
 import pl.marekvogt.cryptocurrency.R
+import kotlinx.android.synthetic.main.main_activity.*
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.navigateUp
+import androidx.navigation.ui.setupWithNavController
 
 class MainActivity : DaggerAppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+
+        navController = findNavController(R.id.navHostFragment)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+
+        toolbar.setupWithNavController(navController, appBarConfiguration)
     }
+
+    override fun onSupportNavigateUp(): Boolean = navigateUp(navController, appBarConfiguration)
 }
