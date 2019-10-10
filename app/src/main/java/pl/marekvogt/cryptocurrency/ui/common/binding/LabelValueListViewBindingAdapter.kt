35bccf61ed8933ba46e8a8e2c9c8e9d0659c1f21
Marek <maarekvogt@gmail.com>
2019-10-10 18:43:55 +0200

    data binding in currency rate details

---
+++ app/src/main/java/pl/marekvogt/cryptocurrency/ui/common/binding/LabelValueListViewBindingAdapter.kt
@@ -0,0 +1,10 @@
+package pl.marekvogt.cryptocurrency.ui.common.binding
+
+import androidx.databinding.BindingAdapter
+import pl.marekvogt.cryptocurrency.ui.common.view.LabelValue
+import pl.marekvogt.cryptocurrency.ui.common.view.LabelValueListView
+
+@BindingAdapter("app:bind")
+fun bindLabelValues(labelValueListView: LabelValueListView, labelValues: List<LabelValue>?) {
+    labelValues?.let { labelValueListView.bind(it) }
+}
--- app/src/main/java/pl/marekvogt/cryptocurrency/ui/detail/CryptoCurrencyDetailsFragment.kt
+++ app/src/main/java/pl/marekvogt/cryptocurrency/ui/detail/CryptoCurrencyDetailsFragment.kt
@@ -5,12 +5,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.os.bundleOf
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import androidx.transition.TransitionInflater
 import dagger.android.support.DaggerFragment
 import kotlinx.android.synthetic.main.fragment_currency_details.*
 import pl.marekvogt.cryptocurrency.R
+import pl.marekvogt.cryptocurrency.databinding.FragmentCurrencyDetailsBinding
+import pl.marekvogt.cryptocurrency.databinding.FragmentCurrencyListBinding
+import pl.marekvogt.cryptocurrency.ui.common.autoCleared
 import pl.marekvogt.cryptocurrency.ui.common.extension.getSerializable
 import pl.marekvogt.cryptocurrency.ui.common.extension.nonNull
 import pl.marekvogt.cryptocurrency.ui.list.CryptoCurrencyRateViewEntity
@@ -27,8 +31,12 @@ class CryptoCurrencyDetailsFragment : DaggerFragment() {
 
     private val viewModel: CryptoCurrencyDetailsViewModel by viewModels { viewModelFactory }
 
+    private var binding by autoCleared<FragmentCurrencyDetailsBinding>()
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_currency_details, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_currency_details, container, false)
+        binding.lifecycleOwner = this
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -45,7 +53,7 @@ class CryptoCurrencyDetailsFragment : DaggerFragment() {
 
     private fun setupViewModel(viewEntity: CryptoCurrencyRateViewEntity) {
         viewModel.getViewState(viewEntity).nonNull().observe(this) { labelValues ->
-            viewLabelValueList.bind(labelValues)
+            binding.labelValues = labelValues
         }
     }
 
